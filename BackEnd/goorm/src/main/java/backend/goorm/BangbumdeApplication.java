4f0dfca74ef2cc2d704fa788b582328304f20981
ChangkiY <53373279+changkiyun@users.noreply.github.com>
2024-07-05 19:14:48 +0900

    first commit

---
+++ BackEnd/goorm/src/main/java/backend/goorm/BangbumdeApplication.java
@@ -0,0 +1,13 @@
+package backend.goorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangbumdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BangbumdeApplication.class, args);
+	}
+
+}
---
+++ BackEnd/goorm/src/test/java/backend/goorm/BangbumdeApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.goorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangbumdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
