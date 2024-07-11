55d8bd4a42a96232bef46753324758b376349d69
cxzlkjhgfdsa <seungheon-i@seungheon-iui-MacBookPro.local>
2024-07-11 14:58:57 +0900

    [Feat] : create Entity { Board, Comment }

--- BackEnd/goorm/src/main/java/backend/goorm/board/model/entity/Comment.java
+++ BackEnd/goorm/src/main/java/backend/goorm/board/model/entity/Comment.java
@@ -1,10 +1,11 @@
 package backend.goorm.board.model.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import backend.goorm.member.model.entity.Member;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -12,9 +13,25 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 @Table(name = "COMMENT")
+@Entity
 public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long commentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "BOARD_ID", nullable = false)
+    private Board boardId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID", nullable = false)
+    private Member memberId;
+
+    @NotBlank
+    private String commentContent;
+
+    private LocalDateTime commentRegDate;
+
+    private boolean commentDeleted = false;
 }
--- BackEnd/goorm/src/main/java/backend/goorm/member/model/entity/Member.java
+++ BackEnd/goorm/src/main/java/backend/goorm/member/model/entity/Member.java
@@ -1,10 +1,9 @@
 package backend.goorm.member.model.entity;
 
 import backend.goorm.member.model.enums.MemberRole;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.cglib.core.Local;
 
@@ -16,27 +15,40 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 @Table(name = "MEMBER")
+@Entity
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
 
+    @Size(max = 50)
     private String loginId;
 
+    @Size(max = 255)
     private String loginPw;
 
+    @Size(max = 20)
+    @NotBlank
     private String memberName;
 
+    @Size(max = 50)
+    @NotBlank
     private String memberEmail;
 
+    @Size(max = 20)
+    @NotBlank
     private String memberNickname;
 
+    @Size(max = 50)
+    @NotBlank
     private String memberPhone;
 
+    @Enumerated(EnumType.STRING)
+    @NotBlank
     private MemberRole memberRole;
 
-    private LocalDateTime memberRegdate;
+    private LocalDateTime memberRegDate;
 
-    private boolean memberInactive;
+    private boolean memberInactive = false;
 }
