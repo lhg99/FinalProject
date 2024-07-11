a44d140246456f5a60b9b5baf115d54cf6a55d22
cxzlkjhgfdsa <cxzlkjhgfdsa0821@gamil.com>
2024-07-11 16:24:40 +0900

    [Test] : test setting file

---
+++ BackEnd/goorm/src/main/java/backend/goorm/board/controller/BoardController.java
@@ -0,0 +1,15 @@
+package backend.goorm.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/board")
+@RequiredArgsConstructor
+public class BoardController {
+
+    @Value("${max.size}")
+    private int maxSize;
+}
