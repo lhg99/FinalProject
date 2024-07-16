6411c476ed2ffa94cce1b5cd501c0322abfc242a
sora <yejh00426@gmail.com>
2024-07-16 14:12:12 +0900

    Feat-Fe/헤더

--- FrontEnd/goorm/src/components/Header/Header.tsx
+++ FrontEnd/goorm/src/components/Header/Header.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import styles from './Header.module.scss';
 
-type Props = {}
-
-function Header({}: Props) {
+const Header: React.FC = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <a href="/">헬스</a>
+      </div>
+      <nav className={styles.nav}>
+        <ul>
+          <li><a href="/Chat">채팅</a></li>
+          <li><a href="/Board">게시판</a></li>
+          <li><a href="/">운동 기록</a></li>
+          <li><a href="/Food">식단 기록</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
