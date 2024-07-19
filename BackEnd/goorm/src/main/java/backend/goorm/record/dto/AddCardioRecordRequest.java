810452675ca3032c24f2a67796336a156e7e17f4
Junyoung Kim <zvpdrnls@gmail.com>
2024-07-19 11:11:40 +0900

    운동 기록 근력, 유산소 컨트롤러 구분하여 구현

---
+++ BackEnd/goorm/src/main/java/backend/goorm/record/dto/AddCardioRecordRequest.java
@@ -0,0 +1,40 @@
+package backend.goorm.record.dto;
+
+import backend.goorm.record.entity.Record;
+import backend.goorm.training.model.entity.Training;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AddCardioRecordRequest {
+
+    private Float caloriesBurned;
+    private Integer durationMinutes;
+    private String intensity;
+    private Float distance;
+    private Float incline;
+    private String memo;
+    private Integer satisfaction;
+    private LocalDate exerciseDate;
+
+    public static Record toEntity(AddCardioRecordRequest request, Training training) {
+        return Record.builder()
+                .training(training)
+                .caloriesBurned(request.getCaloriesBurned())
+                .durationMinutes(request.getDurationMinutes())
+                .intensity(request.getIntensity())
+                .distance(request.getDistance())
+                .incline(request.getIncline())
+                .exerciseDate(request.getExerciseDate())
+                .memo(request.getMemo())
+                .satisfaction(request.getSatisfaction())
+                .build();
+    }
+}
--- BackEnd/goorm/src/main/java/backend/goorm/record/repository/RecordRepository.java
+++ BackEnd/goorm/src/main/java/backend/goorm/record/repository/RecordRepository.java
@@ -1,6 +1,12 @@
-//package backend.goorm.record.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface RecordRepository extends JpaRepository<Record, Long> {
-//}
+package backend.goorm.record.repository;
+
+
+import backend.goorm.record.entity.Record;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RecordRepository extends JpaRepository<Record, Long> {
+    List<Record> findAll();
+    List<Record> findByTraining_TrainingId(Long trainingId);
+}
