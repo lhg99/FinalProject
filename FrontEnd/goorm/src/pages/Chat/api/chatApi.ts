fbcee4865d0568282a8843b4ad2e4e4fec221114
changkiyun <ekzm7845@naver.com>
2024-07-23 22:46:36 +0900

    ✨ [FE/Feat/changki] 1:1 채팅방 요청 URL 수정

--- FrontEnd/goorm/src/pages/Chat/api/chatApi.ts
+++ FrontEnd/goorm/src/pages/Chat/api/chatApi.ts
@@ -3,7 +3,7 @@ import axiosInstance from "../../../api/axiosInstance";
 //채팅룸 목록 가져오기
 export const getChatRoomsByMember = async (loginId: string) => {
   try {
-    const response = await axiosInstance.get(`/members/${loginId}/chatrooms`);
+    const response = await axiosInstance.get(`/chatroom/private/${loginId}`);
     return response.data;
   } catch (error) {
     // console.error('채팅방 불러오기 오류 : chatApi에서 발생', error);
