c7c8fc4afa5011cc4aaab4b7d9aec933c97e3d35
changkiyun <ekzm7845@naver.com>
2024-07-26 17:09:38 +0900

    :sparkles: [FE/Feat/changki] 채팅방 개설 -> 채팅방이동

--- FrontEnd/goorm/src/pages/Chat/api/chatApi.ts
+++ FrontEnd/goorm/src/pages/Chat/api/chatApi.ts
@@ -2,53 +2,69 @@ import axiosInstance from "../../../api/axiosInstance";
 
 //1:1 채팅방 목록 가져오기
 export const getPrivateChatRoomsByMember = async (loginId: string) => {
-  try {
-    const response = await axiosInstance.get(`/chatroom/private/${loginId}`);
-    return response.data;
-  } catch (error) {
-    // console.error('채팅방 불러오기 오류 : chatApi에서 발생', error);
-    throw error;
-  }
+   try {
+      const response = await axiosInstance.get(`/chatroom/private/${loginId}`);
+      return response.data;
+   } catch (error) {
+      // console.error('채팅방 불러오기 오류 : chatApi에서 발생', error);
+      throw error;
+   }
 };
 
 //오픈채팅 목록 가져오기
 export const getPublicChatRoomsByMember = async (loginId: string) => {
-  try {
-    const response = await axiosInstance.get(`/chatroom/public/${loginId}`);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+   try {
+      const response = await axiosInstance.get(`/chatroom/public/${loginId}`);
+      return response.data;
+   } catch (error) {
+      throw error;
+   }
 };
 
 //참여하지 않은 오픈채팅방 목록 가져오기
 export const getPublicChatRoom = async (loginId: string) => {
-  try {
-    const response = await axiosInstance.get(`/chatroom/public/list/${loginId}`);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+   try {
+      const response = await axiosInstance.get(
+         `/chatroom/public/list/${loginId}`
+      );
+      return response.data;
+   } catch (error) {
+      throw error;
+   }
 };
 
 //채팅방 히스토리 가져오기
 export const getChatHistory = async (chatRoomId: number) => {
-    try {
-        const response = await axiosInstance.get(`/history/${chatRoomId}`)
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
+   try {
+      const response = await axiosInstance.get(`/history/${chatRoomId}`);
+      return response.data;
+   } catch (error) {
+      throw error;
+   }
+};
 
 //채팅방 참여하기
-export const joinChatRoom = async (loginId: string, chatRoomId: number) => {
-    try {
-        const response = await axiosInstance.get(`/chatroom/join`)
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
+export const joinChatRoom = async (chatRoomId: number) => {
+   try {
+      const response = await axiosInstance.post(`/chatroom/join`);
+      return response.data;
+   } catch (error) {
+      throw error;
+   }
+};
 
-
+//채팅방 만들기
+export const createChatRoom = async (
+   chatRoomName: string,
+   chatRoomType: string
+) => {
+   try {
+      const response = await axiosInstance.post(`/chatroom`, {
+         chatRoomName,
+         chatRoomType,
+      });
+      return response.data;
+   } catch (error) {
+      throw error;
+   }
+};
