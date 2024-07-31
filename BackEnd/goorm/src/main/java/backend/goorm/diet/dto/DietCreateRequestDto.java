ddb98b6967a35bf2399329203781ea188bdef81d
Junyoung Kim <zvpdrnls@gmail.com>
2024-08-01 00:10:26 +0900

    DietCreateRequestDto 수정

--- BackEnd/goorm/src/main/java/backend/goorm/diet/dto/DietCreateRequestDto.java
+++ BackEnd/goorm/src/main/java/backend/goorm/diet/dto/DietCreateRequestDto.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@NoArgsConstructor
 @Builder
 @Getter
 @Setter
@@ -19,7 +18,6 @@ public class DietCreateRequestDto {
 
     @NoArgsConstructor
     @AllArgsConstructor
-    @NoArgsConstructor
     @Builder
     @Getter
     @Setter
