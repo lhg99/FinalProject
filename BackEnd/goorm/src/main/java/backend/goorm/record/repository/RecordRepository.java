c5b41e11a3b496c1c8550f566c05502c577db218
Junyoung Kim <zvpdrnls@gmail.com>
2024-08-08 12:04:56 +0900

    운동 기록 젠체 조회 페이징 빼기

--- BackEnd/goorm/src/main/java/backend/goorm/record/repository/RecordRepository.java
+++ BackEnd/goorm/src/main/java/backend/goorm/record/repository/RecordRepository.java
@@ -17,6 +17,7 @@ public interface RecordRepository extends JpaRepository<Record, Long> {
     List<Record> findAll();
     List<Record> findByTraining_TrainingId(Long trainingId);
 
+    List<Record> findAllByMember(Member member);
     Page<Record> findAllByMember(Member member, Pageable pageable);
 
     Page<Record> findByExerciseDateBetweenAndMember(LocalDate start, LocalDate end, Member member, Pageable pageable);
