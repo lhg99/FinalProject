e34b9cecb5dce0f2147051b438b7b18ce38c7e11
sora <yejh00426@gmail.com>
2024-08-10 01:20:35 +0900

    [FE/JINHEE] 랜딩, 로그인, 회원가입 수정 완료]

--- FrontEnd/goorm/src/pages/Login/Landing/Landig.tsx
+++ FrontEnd/goorm/src/pages/Login/Landing/Landig.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./Landing.module.scss";
 
 const Landing: React.FC = () => {
   const [h1Loaded, setH1Loaded] = useState(false);
   const [h2Loaded, setH2Loaded] = useState(false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const h1Timer = setTimeout(() => {
@@ -21,19 +24,26 @@ const Landing: React.FC = () => {
     return () => clearTimeout(h2Timer);
   }, []);
 
+  const handleButtonClick = () => {
+    navigate('/login');
+  };
+
   return (
     <div className={styles.pageBackground}>
       <section id="main-title" className={`${styles.section} ${h1Loaded ? styles.loaded : ""}`}>
         <div className={styles.content}>
           <h1>건강한 운동 습관</h1>
           <h2>이디핏과 함께하세요</h2>
+          <div className={styles["button-container"]}>
+            <button onClick={handleButtonClick}>이디와 함께하기</button>
+          </div>
         </div>
       </section>
       <section id="main-button" className={`${styles.section} ${h2Loaded ? styles.loaded : ""}`}>
         <div className={styles.content}>
-          <div className={styles["button-container"]}>
+          {/* <div className={styles["button-container"]}>
             <button>지금 바로 시작해 보세요</button>
-          </div>
+          </div> */}
         </div>
       </section>
     </div>
