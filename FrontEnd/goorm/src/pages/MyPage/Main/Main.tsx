c858159e6ee02323f2ff65f5630271bced4802ad
sora <yejh00426@gmail.com>
2024-08-10 03:37:03 +0900

    [FE/JINHEE] 메인 페이지 버튼 핸들 추가

--- FrontEnd/goorm/src/pages/MyPage/Main/Main.tsx
+++ FrontEnd/goorm/src/pages/MyPage/Main/Main.tsx
@@ -1,13 +1,23 @@
 import React, { useCallback, useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // useNavigate 훅을 임포트합니다.
 import styles from './Main.module.scss';
 import MyCalendar from '../../Exercise/components/Date/Calendar';
 
 const MainPage: React.FC = () => {
   const [dateInfo, setDateInfo] = useState<{ year: number, month: number, day: number, weekday: string } | null>(null);
+  const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 함수를 가져옵니다.
 
   const handleDateChange = useCallback((info: { year: number, month: number, day: number, weekday: string }) => {
     setDateInfo(info);
   }, []);
+
+  const handleExerciseRecordClick = () => {
+    navigate('/exercise');
+  };
+
+  const handleDietRecordClick = () => {
+    navigate('/food');
+  };
 
   return (
     <div className={styles.pageBackground}>
@@ -17,12 +27,12 @@ const MainPage: React.FC = () => {
         </div>
         <div className={styles.rightPane}>
           <div className={styles.buttons}>
-            <button className={styles.button1}>
+            <button className={styles.button1} onClick={handleExerciseRecordClick}>
               <div className={styles.buttonContent}>
                 <span>운동 기록</span>
               </div>
             </button>
-            <button className={styles.button2}>
+            <button className={styles.button2} onClick={handleDietRecordClick}>
               <div className={styles.buttonContent}>
                 <span>식단 기록</span>
               </div>
