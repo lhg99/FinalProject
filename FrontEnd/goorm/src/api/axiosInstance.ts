378c5434919c8e32089c9b708d9e48894a4af870
seonghee <comely_15@naver.com>
2024-08-10 17:10:47 +0900

    [FE/seonghee]main page merge

--- FrontEnd/goorm/src/api/axiosInstance.ts
+++ FrontEnd/goorm/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://final-project-app-env.eba-xdjqmujd.ap-northeast-2.elasticbeanstalk.com/api',
+  baseURL: 'http://localhost:8080/api',
   timeout: 10000, 
   headers: {
     'Authorization': `Bearer ${localStorage.getItem('token')}`, // 로컬 스토리지에서 토큰을 가져와 헤더에 포함
