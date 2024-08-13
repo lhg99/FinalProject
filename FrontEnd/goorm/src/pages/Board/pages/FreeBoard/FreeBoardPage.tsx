5e2069f414aa4d6c76076419088844510c7fa5a9
lhg99 <lee991229@naver.com>
2024-08-13 21:01:00 +0900

    인덱싱 수정

--- FrontEnd/goorm/src/pages/Board/pages/FreeBoard/FreeBoardPage.tsx
+++ FrontEnd/goorm/src/pages/Board/pages/FreeBoard/FreeBoardPage.tsx
@@ -64,7 +64,7 @@ const FreeBoardPage: React.FC = () => {
           {currentPosts.map((post, index) => (
             <tr key={post.boardId} className={styles.tableRow}>
               <td className={styles.tableCell}>
-              {totalPages - ((currentPage - 2) * postsPerPage + (index + 7)) }
+              {index + 1 + (currentPage - 1) * postsPerPage}
               </td>              
               <td className={styles.tableCell}>{categoryMap[post.boardCategory] || post.boardCategory}</td>
               <td className={styles.titleCell} onClick={() => navigate(`/Board/free/post/${post.boardId}`)}>
